50967c433ca8d2b20c174ee70721b6f7ebf1b48d
Mabrey <matthewra95@gmail.com>
2019-11-10 21:47:08 -0800

    Created api endpoints for creating a chatroom and user identification

--- server/server.js
+++ server/server.js
@@ -1,17 +1,42 @@
 const express = require('express');
-const app = express();
 const http = require('http');
-const port = process.env.PORT || 5000;
 const WebSocket = require('ws');
+const shortid = require('shortid');
+const app = express();
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
 let users = 0;
 
-wss.on('connection', (ws) => {
-    ws.on('message', (message) =>{
-        console.log('recieved: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+let rooms = {};
+
+let clients = {};
+
+// wss.on('connection', (ws) => {
+//     ws.on('message', (message) =>{
+//         console.log('recieved: %s', message);
+//         ws.send(`Hello, you sent -> ${message}`);
+//     });
+//     ws.send('Hi, I am a ws server!');
+// });
+
+let room = (member, id) => {
+    return {member:{member}, roomID: id}
+}
+
+let member = (memberID, connection='') => {
+    return {memberID: memberID, connection: connection}
+}
+
+
+wss.on('connection', (ws, req) => {
+    ws.on('message', function incoming(message){
+        wss.clients.forEach(function each(client) {
+            if(client !== ws && client.readyState === WebSocket.OPEN){
+                client.send(message);
+            }
+        } )
     });
     ws.send('Hi, I am a ws server!');
 });
@@ -23,9 +48,14 @@ app.get('/express_backend', (req, res) => {
     res.send({express: 'Your Express Backend Works'});
 });
 
-app.get('/get_user_number', (req, res) => {
-    users++;
-    res.send({express: `${users}`})
+app.get('/create_user_id', (req, res) => {
+    let userID = shortid.generate();
+    res.send({userID: `${userID}`})
 });
 
-
+app.get('/create_room', (req, res) => {
+    let roomID = shortid.generate();
+    let member = member(req.memberID);
+    let room = room(req.clientID, roomID)
+    res.send({roomID: `${roomID}`})
+});
